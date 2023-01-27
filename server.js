0f0abd6736037adc48f495e387970413dcb5faf0
JBourton <joshua.bourton16@gmail.com>
2023-01-27 12:22:40 +0000

    Import bodyparser and cors packages

--- server.js
+++ server.js
@@ -20,8 +20,12 @@ app.listen(8080);
 
 
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const port = 8080;
 
 app.use(express.static('client'));
 
@@ -46,3 +50,36 @@ app.listen(8080, ()=> {
   console.log("listening on port 8080");
 })
 */
+
+/* With thanks to https://dev.to/gbudjeakp/how-to-connect-your-client-side-to-your-server-side-using-node-and-express-2i71 for information on how to user body parser and cors packages */
+
+// We are using our packages here
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true})); 
+app.use(cors())
+
+//You can use this to check if your server is working
+app.get('/h', (req, res)=>{
+  res.send("Welcome to your server")
+})
+
+
+//Route that handles login logic
+app.post('/login', (req, res) =>{
+  console.log(req.body.username); 
+  console.log(req.body.password);
+})
+
+//Route that handles signup logic
+app.post('/signup', (req, res) =>{
+console.log(req.body.title);
+console.log(req.body.username);
+console.log(req.body.password); 
+})
+
+//Start your server on a specified port
+app.listen(port, ()=>{
+  console.log(`Server is runing on port ${port}`)
+})
